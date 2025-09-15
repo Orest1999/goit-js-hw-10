8abe5e1c6fe588f2e6fffb355fa3f1da662ebe5f
orestpol <orest.polagnyuk@gmail.com>
2025-09-15 11:06:24 +0300

    fewfwefwfwq

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,48 +2,51 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+
 const form = document.querySelector(".form");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+form.addEventListener("submit", handlerSubmit);
 
-  const delay = Number(form.elements.delay.value);
-  const state = form.elements.state.value;
 
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (state === "fulfilled") {
-        resolve(delay);
-      } else {
-        reject(delay);
-      }
-    }, delay);
-  });
 
-  promise
-    .then((delay) => {
-      iziToast.success({
-        title: "Success",
-        message: `✅ Fulfilled promise in ${delay}ms`,
-        position: "topRight",
-      });
-    })
-    .catch((delay) => {
-      iziToast.error({
-        title: "Error",
-        message: `❌ Rejected promise in ${delay}ms`,
-        position: "topRight",
-      });
-    })
+
+function handlerSubmit(event) {
+    event.preventDefault();
+    const delay = Number(form.elements.delay.value);
+    const fielfildValue = form.elements.state.value;
   
-  function showToast(message, bgColor) {
-    iziToast.show({
-      message,
-      position: "topRight",
-      icon: "",
-      backgroundColor: bgColor,
-      messageColor: "#ffffff",
-    });
 
-  }
-});
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (fielfildValue === "fulfilled") {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+
+    });
+    promise
+        .then((delay) => {
+            showToast(`✅ Fulfilled promise in ${delay}ms`, 'green');
+        })
+        .catch((delay) => {
+            showToast(`❌ Rejected promise in ${delay}ms`, 'red')
+        });
+
+    form.reset();
+
+    
+
+}
+
+    function showToast(message, bgColor) {
+        iziToast.show({
+            message,
+            position: "topRight",
+            icon: "", 
+            backgroundColor: bgColor,
+            messageColor: "#ffffff",
+        });
+
+    };
